dceedcd3067207483cc4bf2a44e387015d5effdf
Magombo <Uo258418@uniovi.es>
2019-10-04 15:32:04 +0200

    created necessary queries for ChangeEnhancedResultSet

--- ChangeEnhancedResultSet/src/main/Main.java
+++ ChangeEnhancedResultSet/src/main/Main.java
@@ -10,6 +10,9 @@ import util.DbUtil;
 public class Main {
 	
 	private final static String GET_TABLE = DbUtil.getQuery("SQL_GET_TABLE");
+	private final static String NEW_ROW = DbUtil.getQuery("SQL_NEW_ROW");
+	private final static String UPDATE_ROW1 = DbUtil.getQuery("SQL_UPDATE_ROW1");
+	private final static String DELETE_ID2 = DbUtil.getQuery("SQL_DELETE_ID2");
 	private final static int SECONDS_TO_SLEEP = 20;
 
 	public static void main(String[] args) {
@@ -18,6 +21,9 @@ public class Main {
 			Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 			ResultSet rs = stmt.executeQuery(GET_TABLE);
 			display(rs);
+			stmt.executeUpdate(NEW_ROW);
+			stmt.executeUpdate(UPDATE_ROW1);
+			stmt.executeUpdate(DELETE_ID2);
 			Thread.sleep(SECONDS_TO_SLEEP * 1000);
 			display(rs);
 			con.close();
